c8fcaa11b5d771c7d98e846eb51a31b5674b801e
Usman Rafiq <usmanrafiq57@gmail.com>
2026-01-12 11:12:44 +0000

    functions added

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+
+function add5andy(y) {console.log(5 + y)}
+add5andy(50)
+add5andy(100)
+
+const usernumber = prompt("Enter a number:")
+console.log (usernumber)
+
+const friendsnumber = 55
+function alerttheuser (number) {
+    alert ("your number was " + number + " and my number is " + friendsnumber) }
+alerttheuser(usernumber)
+
+function sum(a, b) {
+    console.log(a + b)
+}
+
+sum(5, 10)
+sum(20, 30)
+sum(100, 200)
