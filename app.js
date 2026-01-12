d3bb579ab0bfb08a9bafbdf25c94df107ee17772
Usman Rafiq <usmanrafiq57@gmail.com>
2026-01-12 14:01:33 +0000

    return

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ console.log (usernumber)
 
 const friendsnumber = 55
 function alerttheuser (number) {
-    alert ("your number was " + number + " and my number is " + friendsnumber) }
+    alert ("your number was " + number + "!") }
 alerttheuser(usernumber)
+alerttheuser(friendsnumber)
 
 function sum(a, b) {
     console.log(a + b)
@@ -19,3 +20,20 @@ function sum(a, b) {
 sum(5, 10)
 sum(20, 30)
 sum(100, 200)
+
+
+function calculate (a, b, operator) {
+    if (operator === "+") {
+        return a + b
+    } else if (operator === "-") {
+        return a - b
+    } else if (operator === "*") {
+        return a * b
+    } else if (operator === "/") {
+        return a / b 
+} }
+
+console.log(calculate(10, 5, "+"))
+console.log(calculate(10, 5, "-"))
+console.log(calculate(10, 5, "*"))
+console.log(calculate(10, 5, "/"))
